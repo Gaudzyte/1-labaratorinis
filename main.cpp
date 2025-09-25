5b7facdf87bc3f803886d82e20e7a361a99995f6
Gaudzyte <gaudzyteurte@gmail.com>
2025-09-25 11:56:22 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,44 +4,46 @@
 #include <iomanip>
 #include <algorithm>
 #include <limits>
-#include <random> 
+#include <random>
 #include <fstream>
 
 using namespace std;
-using std::cout;
 using std::cin;
+using std::cout;
 using std::end;
-using std::string;
-using std::vector;
-using std::setw;
 using std::left;
 using std::right;
+using std::setw;
+using std::string;
+using std::vector;
 
 struct Studentas
 {
     string var;
     string pav;
-    vector <int> paz;
+    vector<int> paz;
     int egz;
     float gal_vid;
-    float gal_med; 
+    float gal_med;
 };
 
-double median(vector<int> paz) {
+double median(vector<int> paz)
+{
     sort(paz.begin(), paz.end());
     int n = paz.size();
-    if (n % 2 == 0) 
-        return (paz[n/2 - 1] + paz[n/2]) / 2.0;
-    else 
-        return paz[n/2];
+    if (n % 2 == 0)
+        return (paz[n / 2 - 1] + paz[n / 2]) / 2.0;
+    else
+        return paz[n / 2];
 }
 
-Studentas Stud_iv(){
+Studentas Stud_iv()
+{
     Studentas Pirmas;
     cout << "Iveskite studento duomenis." << endl;
-    cout << "Vardas: "; 
-    cin >> Pirmas.var; 
-    cout << "Pavarde: "; 
+    cout << "Vardas: ";
+    cin >> Pirmas.var;
+    cout << "Pavarde: ";
     cin >> Pirmas.pav;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
@@ -50,32 +52,40 @@ Studentas Stud_iv(){
     int suma = 0;
     int tusti = 0;
 
-    while (true) {
+    while (true)
+    {
         getline(cin, eilute);
 
-        if (eilute.empty()) {
+        if (eilute.empty())
+        {
             tusti++;
-            if (tusti == 1) break; 
+            if (tusti == 1)
+                break;
             continue;
-        } else {
-            tusti = 0; 
         }
-        int laik_paz = stoi(eilute); 
+        else
+        {
+            tusti = 0;
+        }
+        int laik_paz = stoi(eilute);
         Pirmas.paz.push_back(laik_paz);
         suma += laik_paz;
     }
 
-    cout << "Iveskite egzamino pazymi: "; 
+    cout << "Iveskite egzamino pazymi: ";
     cin >> Pirmas.egz;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-    if (!Pirmas.paz.empty()) {
+    if (!Pirmas.paz.empty())
+    {
         double vid = double(suma) / double(Pirmas.paz.size());
         double med = median(Pirmas.paz);
 
         Pirmas.gal_vid = vid * 0.4 + 0.6 * Pirmas.egz;
         Pirmas.gal_med = med * 0.4 + 0.6 * Pirmas.egz;
-    } else {
+    }
+    else
+    {
         Pirmas.gal_vid = 0.6 * Pirmas.egz;
         Pirmas.gal_med = 0.6 * Pirmas.egz;
     }
@@ -83,22 +93,26 @@ Studentas Stud_iv(){
     return Pirmas;
 }
 
-Studentas Stud_rand() {
+Studentas Stud_rand()
+{
     Studentas S;
     cout << "Iveskite studento duomenis." << endl;
-    cout << "Vardas: "; cin >> S.var;
-    cout << "Pavarde: "; cin >> S.pav;
+    cout << "Vardas: ";
+    cin >> S.var;
+    cout << "Pavarde: ";
+    cin >> S.pav;
 
     random_device rd;
     mt19937 gen(rd());
-    uniform_int_distribution<> paz_dist(1, 10);   
-    uniform_int_distribution<> egz_dist(1, 10);   
-    uniform_int_distribution<> kiek_dist(1, 7);  
+    uniform_int_distribution<> paz_dist(1, 10);
+    uniform_int_distribution<> egz_dist(1, 10);
+    uniform_int_distribution<> kiek_dist(1, 7);
 
     int suma = 0;
     int kiek = kiek_dist(gen);
 
-    for (int i = 0; i < kiek; i++) {
+    for (int i = 0; i < kiek; i++)
+    {
         int paz = paz_dist(gen);
         S.paz.push_back(paz);
         suma += paz;
@@ -114,29 +128,36 @@ Studentas Stud_rand() {
     return S;
 }
 
-Studentas Stud_file(ifstream &fin) {
+Studentas Stud_file(ifstream &fin)
+{
     Studentas S;
     S.paz.clear();
-    if (!(fin >> S.var >> S.pav)) return S;
+    if (!(fin >> S.var >> S.pav))
+        return S;
 
     vector<int> laikini;
     int x;
-    while (fin.peek() != '\n' && fin >> x) {
+    while (fin.peek() != '\n' && fin >> x)
+    {
         laikini.push_back(x);
     }
 
-    if (!laikini.empty()) {
+    if (!laikini.empty())
+    {
         S.egz = laikini.back();
-        laikini.pop_back(); 
+        laikini.pop_back();
         S.paz = laikini;
 
         int suma = 0;
-        for (auto p : S.paz) suma += p;
+        for (auto p : S.paz)
+            suma += p;
         double vid = double(suma) / S.paz.size();
         double med = median(S.paz);
         S.gal_vid = vid * 0.4 + 0.6 * S.egz;
         S.gal_med = med * 0.4 + 0.6 * S.egz;
-    } else {
+    }
+    else
+    {
         S.egz = 0;
         S.gal_vid = 0.6 * S.egz;
         S.gal_med = 0.6 * S.egz;
@@ -157,56 +178,67 @@ int main()
     cout << "Pasirinkimas: ";
     cin >> pasirinkimas;
 
-    while (pasirinkimas != 1 && pasirinkimas != 2 && pasirinkimas != 3) {
+    while (pasirinkimas != 1 && pasirinkimas != 2 && pasirinkimas != 3)
+    {
         cout << "Neteisingas pasirinkimas! Bandykite dar karta: ";
         cin >> pasirinkimas;
     }
 
-    if (pasirinkimas == 1) {
+    if (pasirinkimas == 1)
+    {
         int m;
         cout << "Kiek studentu grupeje? ";
         cin >> m;
-        for (int z = 0; z < m; z++) {
+        for (int z = 0; z < m; z++)
+        {
             Grupe.push_back(Stud_iv());
         }
     }
-    else if (pasirinkimas == 2) {
+    else if (pasirinkimas == 2)
+    {
         int m;
         cout << "Kiek studentu grupeje? ";
         cin >> m;
-        for (int z = 0; z < m; z++) {
+        for (int z = 0; z < m; z++)
+        {
             Grupe.push_back(Stud_rand());
         }
     }
-else if (pasirinkimas == 3) {
-    string failoPav;
-    cout << "Iveskite failo pavadinima: ";
-    cin >> failoPav;
+    else if (pasirinkimas == 3)
+    {
+        string failoPav;
+        cout << "Iveskite failo pavadinima: ";
+        cin >> failoPav;
 
-    ifstream fin(failoPav);
-    if (!fin) {
-        cout << "Nepavyko atidaryti failo " << failoPav << endl;
-        return 1;
+        ifstream fin(failoPav);
+        if (!fin)
+        {
+            cout << "Nepavyko atidaryti failo " << failoPav << endl;
+            return 1;
+        }
+
+        string headerLine;
+        getline(fin, headerLine);
+
+        while (fin.peek() != EOF)
+        {
+            Grupe.push_back(Stud_file(fin));
+        }
+
+        cout << "Duomenys nuskaityti iš failo " << failoPav << "." << endl;
     }
-
-    string headerLine;
-    getline(fin, headerLine); 
-
-    while (fin.peek() != EOF) {
-        Grupe.push_back(Stud_file(fin));
-    }
-
-    cout << "Duomenys nuskaityti iš failo " << failoPav << "." << endl;
-}
+    sort(Grupe.begin(), Grupe.end(), [](const Studentas &a, const Studentas &b)
+         { return a.var < b.var; });
     cout << endl;
-    cout << left << setw(15) << "Pavarde"
+    cout << left << setw(20) << "Pavarde"
          << setw(15) << "Vardas"
          << setw(20) << "Galutinis (Vid.)"
          << setw(20) << "Galutinis (Med.)" << endl;
-    cout << string(70, '-') << endl;
+    cout << string(80, '-') << endl;
 
-    for (auto &past : Grupe) {
-        cout << left << setw(15) << past.pav
+    for (auto &past : Grupe)
+    {
+        cout << left << setw(20) << past.pav
              << setw(15) << past.var
              << fixed << setprecision(2)
              << setw(20) << past.gal_vid
