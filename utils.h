4023f64756df8fc5af52ee1e13be5e7bb55991dd
Gaudzyte <gaudzyteurte@gmail.com>
2025-10-02 11:17:18 +0300

    Create utils.h

---
+++ utils.h
@@ -0,0 +1,11 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <vector>
+#include <string>
+#include "studentas.h"
+
+void issaugotiRezultatus(const std::vector<Studentas>& vargsiukai,
+                         const std::vector<Studentas>& kietiakiai);
+
+#endif
