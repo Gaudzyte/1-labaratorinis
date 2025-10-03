acf5f4145328bfbeb8199917f3435302d3e260e4
Gaudzyte <gaudzyteurte@gmail.com>
2025-10-03 09:26:03 +0300

    Update utils.cpp

--- utils.cpp
+++ utils.cpp
@@ -1,43 +1,71 @@
 #include "utils.h"
-#include <fstream>
-#include <iomanip>
 
 using namespace std;
 
-void issaugotiRezultatus(const vector<Studentas>& vargsiukai,
-                         const vector<Studentas>& kietiakiai)
+void issaugotiRezultatus(const vector<Studentas> &vargsiukai,
+                         const vector<Studentas> &kietiakiai)
 {
-    ofstream out1("vargsiukai.txt");
-    out1 << left << setw(20) << "Pavarde"
-         << setw(15) << "Vardas"
-         << setw(20) << "Galutinis (Vid.)"
-         << setw(20) << "Galutinis (Med.)" << endl;
-    out1 << string(80, '-') << endl;
+     if (vargsiukai.size() != 0)
+     {
+          ofstream out1("vargsiukai.txt");
+          out1 << left << setw(20) << "Pavarde"
+               << setw(15) << "Vardas"
+               << setw(20) << "Galutinis (Vid.)"
+               << setw(20) << "Galutinis (Med.)" << endl;
+          out1 << string(80, '-') << endl;
 
-    for (auto &s : vargsiukai)
-    {
-        out1 << left << setw(20) << s.pav
-             << setw(15) << s.var
-             << fixed << setprecision(2)
-             << setw(20) << s.gal_vid
-             << setw(20) << s.gal_med << endl;
-    }
-    out1.close();
+          for (auto &s : vargsiukai)
+          {
+               out1 << left << setw(20) << s.pav
+                    << setw(15) << s.var
+                    << fixed << setprecision(2)
+                    << setw(20) << s.gal_vid
+                    << setw(20) << s.gal_med << endl;
+          }
+          out1.close();
+     }
 
-    ofstream out2("kietiakiai.txt");
-    out2 << left << setw(20) << "Pavarde"
-         << setw(15) << "Vardas"
-         << setw(20) << "Galutinis (Vid.)"
-         << setw(20) << "Galutinis (Med.)" << endl;
-    out2 << string(80, '-') << endl;
+     ofstream out2("kietiakiai.txt");
+     out2 << left << setw(20) << "Pavarde"
+          << setw(15) << "Vardas"
+          << setw(20) << "Galutinis (Vid.)"
+          << setw(20) << "Galutinis (Med.)" << endl;
+     out2 << string(80, '-') << endl;
 
-    for (auto &s : kietiakiai)
-    {
-        out2 << left << setw(20) << s.pav
-             << setw(15) << s.var
-             << fixed << setprecision(2)
-             << setw(20) << s.gal_vid
-             << setw(20) << s.gal_med << endl;
-    }
-    out2.close();
+     for (auto &s : kietiakiai)
+     {
+          out2 << left << setw(20) << s.pav
+               << setw(15) << s.var
+               << fixed << setprecision(2)
+               << setw(20) << s.gal_vid
+               << setw(20) << s.gal_med << endl;
+     }
+     out2.close();
+}
+
+int extractNumber(const string &pav)
+{
+     string digits;
+     for (char c : pav)
+     {
+          if (isdigit(c))
+               digits += c;
+     }
+     if (digits.empty())
+          return 0;
+     return stoi(digits);
+}
+
+void rusiuoti(vector<Studentas> &grupe, int pasirinkimas)
+{
+     sort(grupe.begin(), grupe.end(), [pasirinkimas](const Studentas &a, const Studentas &b)
+          {
+            if (pasirinkimas == 1) {
+               return a.var < b.var;
+            } else if (pasirinkimas == 2) {
+               return a.pav < b.pav;
+            } else if (pasirinkimas == 3) {
+               return a.gal_vid < b.gal_vid;
+            }
+            else return false; });
 }
