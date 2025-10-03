68c395430319218d9e42ce10241fc4419b46ddba
Gaudzyte <gaudzyteurte@gmail.com>
2025-10-03 09:26:25 +0300

    Update utils.h

--- utils.h
+++ utils.h
@@ -1,11 +1,19 @@
-#ifndef UTILS_H
-#define UTILS_H
+//#ifndef UTILS_H
+//#define UTILS_H
 
 #include <vector>
 #include <string>
+#include <fstream>
+#include <iomanip>
+#include <algorithm>
 #include "studentas.h"
+using std::sort;
+using std::string;
+using std::vector;
 
 void issaugotiRezultatus(const std::vector<Studentas>& vargsiukai,
                          const std::vector<Studentas>& kietiakiai);
 
-#endif
+void rusiuoti(vector<Studentas>& grupe, int pasirinkimas);
+int extractNumber(const string& pav);
+//#endif
