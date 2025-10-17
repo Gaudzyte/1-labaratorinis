505fb7f3661c418d3b755132ce9777e5f474ef59
Gaudzyte <gaudzyteurte@gmail.com>
2025-10-17 08:47:35 +0300

    Refactor issaugotiRezultatus function and clean up code

--- utils.h
+++ utils.h
@@ -1,6 +1,3 @@
-//#ifndef UTILS_H
-//#define UTILS_H
-
 #include <list>
 #include <vector>
 #include <string>
@@ -11,10 +8,59 @@
 using std::sort;
 using std::string;
 using std::vector;
+using std::left;
+using std::ofstream;
+using std::setw;
+using std::endl;
+using std::fixed;
+using std::setprecision;
 
+/*
 void issaugotiRezultatus(const std::vector<Studentas>& vargsiukai,
                          const std::vector<Studentas>& kietiakiai);
-
+*/
 void rusiuoti(vector<Studentas>& grupe, int pasirinkimas);
 int extractNumber(const string& pav);
 //#endif
+
+template <typename T>
+void issaugotiRezultatus(const T &vargsiukai,
+                         const T &kietiakiai)
+{
+     if (vargsiukai.size() != 0)
+     {
+          ofstream out1("vargsiukai.txt");
+          out1 << left << setw(20) << "Pavarde"
+               << setw(15) << "Vardas"
+               << setw(20) << "Galutinis (Vid.)"
+               << setw(20) << "Galutinis (Med.)" << endl;
+          out1 << string(80, '-') << endl;
+
+          for (auto &s : vargsiukai)
+          {
+               out1 << left << setw(20) << s.pav
+                    << setw(15) << s.var
+                    << fixed << setprecision(2)
+                    << setw(20) << s.gal_vid
+                    << setw(20) << s.gal_med << endl;
+          }
+          out1.close();
+     }
+
+     ofstream out2("kietiakiai.txt");
+     out2 << left << setw(20) << "Pavarde"
+          << setw(15) << "Vardas"
+          << setw(20) << "Galutinis (Vid.)"
+          << setw(20) << "Galutinis (Med.)" << endl;
+     out2 << string(80, '-') << endl;
+
+     for (auto &s : kietiakiai)
+     {
+          out2 << left << setw(20) << s.pav
+               << setw(15) << s.var
+               << fixed << setprecision(2)
+               << setw(20) << s.gal_vid
+               << setw(20) << s.gal_med << endl;
+     }
+     out2.close();
+}
