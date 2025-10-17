9a7b2dde561518862443ff565c8bbbf0b7fc68bc
Gaudzyte <gaudzyteurte@gmail.com>
2025-10-17 08:48:13 +0300

    Rezultatus function

--- utils.cpp
+++ utils.cpp
@@ -2,47 +2,6 @@
 
 using namespace std;
 
-void issaugotiRezultatus(const vector<Studentas> &vargsiukai,
-                         const vector<Studentas> &kietiakiai)
-{
-     if (vargsiukai.size() != 0)
-     {
-          ofstream out1("vargsiukai.txt");
-          out1 << left << setw(20) << "Pavarde"
-               << setw(15) << "Vardas"
-               << setw(20) << "Galutinis (Vid.)"
-               << setw(20) << "Galutinis (Med.)" << endl;
-          out1 << string(80, '-') << endl;
-
-          for (auto &s : vargsiukai)
-          {
-               out1 << left << setw(20) << s.pav
-                    << setw(15) << s.var
-                    << fixed << setprecision(2)
-                    << setw(20) << s.gal_vid
-                    << setw(20) << s.gal_med << endl;
-          }
-          out1.close();
-     }
-
-     ofstream out2("kietiakiai.txt");
-     out2 << left << setw(20) << "Pavarde"
-          << setw(15) << "Vardas"
-          << setw(20) << "Galutinis (Vid.)"
-          << setw(20) << "Galutinis (Med.)" << endl;
-     out2 << string(80, '-') << endl;
-
-     for (auto &s : kietiakiai)
-     {
-          out2 << left << setw(20) << s.pav
-               << setw(15) << s.var
-               << fixed << setprecision(2)
-               << setw(20) << s.gal_vid
-               << setw(20) << s.gal_med << endl;
-     }
-     out2.close();
-}
-
 int extractNumber(const string &pav)
 {
      string digits;
