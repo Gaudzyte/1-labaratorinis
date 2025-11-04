64219686bf961ccdcf0e7bdd0e45c52c54188c40
Gaudzyte <gaudzyteurte@gmail.com>
2025-11-04 20:38:37 +0200

    Add file name parameter to distribution strategies

--- mylib.h
+++ mylib.h
@@ -11,15 +11,20 @@
 std::vector<Studentas> NuskaitytiVector(const std::string& failas);
 
 // Strategijos
-void Paskirstymas_vector_1_strategija(const std::vector<Studentas>& Grupe, const int irasu_sk);
-void Paskirstymas_vector_2_strategija(std::vector<Studentas> Grupe, const int irasu_sk);
-void Paskirstymas_vector_3_strategija(std::vector<Studentas>& Grupe, const int irasu_sk);
-void Paskirstymas_list_1_strategija(const std::list<Studentas>& Grupe, const int irasu_sk);
-void Paskirstymas_list_2_strategija(std::list<Studentas> Grupe, const int irasu_sk);
-void Paskirstymas_list_3_strategija(std::list<Studentas>& Grupe, const int irasu_sk);
+void Paskirstymas_vector_1_strategija(const std::vector<Studentas>& Grupe, const int irasu_sk, const std::string& failo_vardas);
+void Paskirstymas_vector_2_strategija(std::vector<Studentas> Grupe, const int irasu_sk, const std::string& failo_vardas);
+void Paskirstymas_vector_3_strategija(std::vector<Studentas>& Grupe, const int irasu_sk, const std::string& failo_vardas);
+void Paskirstymas_list_1_strategija(const std::list<Studentas>& Grupe, const int irasu_sk, const std::string& failo_vardas);
+void Paskirstymas_list_2_strategija(std::list<Studentas> Grupe, const int irasu_sk, const std::string& failo_vardas);
+void Paskirstymas_list_3_strategija(std::list<Studentas>& Grupe, const int irasu_sk, const std::string& failo_vardas);
+
 
 // Testavimas
 void TestavimasIsFailo(const std::string& failas, int irasu_sk);
+
+// Spausdinimas
+template <typename T>
+void Spausdinimas(const T &Spausd_gr, const int& strat, const std::string& failo_vardas);
 
 
 #endif
